84f45c050f94528bf1d93335c95e75f96d42458d
Noah Richardson <RichNoah983@GoTVDSB.ca>
2020-11-09 21:41:18 -0500

    Add project files.

---
+++ Part 5 - Hurricane Project/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Part_5___Hurricane_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hur_cat;
+
+            Console.WriteLine("Please enter a category of hurricane form 1-5.");
+
+            hur_cat switch (1)
+            {
+
+            };
+        }
+    }
+}
